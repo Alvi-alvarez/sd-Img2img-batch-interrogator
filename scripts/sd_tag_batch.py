c625bd4d546e689bb8b02513dac66895bab804f0
Santiago Alvarez <santiagodamian1997@gmail.com>
2023-03-27 18:28:49 -0300

    add code

---
+++ scripts/sd_tag_batch.py
@@ -0,0 +1,63 @@
+import gradio as gr
+import torch
+from modules import scripts, devices, lowvram, shared
+from clip_interrogator import Config, Interrogator
+from modules.processing import process_images
+
+ci = None
+
+
+def unload():
+    global ci
+    if ci is not None:
+        print("Offloading CLIP Interrogator...")
+        ci.caption_model = ci.caption_model.to(devices.cpu)
+        ci.clip_model = ci.clip_model.to(devices.cpu)
+        ci.caption_offloaded = True
+        ci.clip_offloaded = True
+        devices.torch_gc()
+
+
+class Script(scripts.Script):
+    def title(self):
+        return "Img2img batch interrogator"
+
+    def show(self, is_img2img):
+        return is_img2img
+
+    def ui(self, is_img2img):
+        with gr.Blocks():
+            in_front = gr.Checkbox(label="Prompt in front")
+            mode = gr.Dropdown(
+                ["classic", "fast"], label="mode", value="classic"
+            )
+            btn = gr.Button(value="unload models")
+            btn.click(unload)
+            return [in_front, mode]
+
+    def run(self, p, in_front, mode):
+        global ci
+        if ci is None:
+            config = Config(
+                device=devices.get_optimal_device(),
+                cache_path='models/clip-interrogator',
+                clip_model_name="ViT-L-14/openai",
+                caption_model_name="blip-base"
+            )
+            ci = Interrogator(config)
+        try:
+            if shared.cmd_opts.lowvram or shared.cmd_opts.medvram:
+                lowvram.send_everything_to_cpu()
+                devices.torch_gc()
+            if mode == "classic":
+                prompt = ci.interrogate_classic(p.init_images[0])
+            elif mode == "fast":
+                prompt = ci.interrogate_fast(p.init_images[0])
+            if in_front:
+                p.prompt = f"{p.prompt}, {prompt}"
+            else:
+                p.prompt = f"{prompt}, {p.prompt}"
+            print(prompt)
+        except RuntimeError as e:
+            print(e)
+        return process_images(p)
